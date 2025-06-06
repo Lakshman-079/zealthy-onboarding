b05df7c342ec6b8735ced94fc7352d1ab5709d41
Lakshman-079 <ltalluri@stevens.edu>
2025-06-06 16:44:21 -0400

    Fix MongoDB URI and update axios URLs

--- server/server.js
+++ server/server.js
@@ -9,10 +9,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI)
+
 
 const User = mongoose.model("User", new mongoose.Schema({
   email: String,
