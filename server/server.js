918a905683eb30065ae71a8cb5233a8a83393fd8
Lakshman-079 <ltalluri@stevens.edu>
2025-06-06 16:15:48 -0400

    server render

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/onboarding", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
